faf7c0bdb6feb1723021b7c15dad6dd26a22e535
Zheoden <schifferjulian@gmail.com>
2022-03-21 20:22:30 -0300

    first commit

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Pizzas.API.Models;
+
+namespace Pizzas.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PizzaController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<Pizza> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Pizza
+            {
+                Descripcion = "Con salsa de tomate y queso",
+                Id = 1,
+                Importe = 300,
+                LibreGluten = false,
+                Nombre = "Muzza Individual"
+            })
+            .ToArray();
+        }
+    }
+}
